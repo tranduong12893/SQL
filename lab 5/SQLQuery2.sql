bea4550e2054665c954044d09603e219b80f4b87
tranduong12893 <73330600+tranduong12893@users.noreply.github.com>
2021-01-26 22:41:48 +0700

    LAB5

---
+++ lab 5/SQLQuery2.sql
@@ -0,0 +1,90 @@
+﻿USE  QuanLyBanHang
+GO
+CREATE TABLE Company(
+	ComID INT PRIMARY KEY IDENTITY,
+	ComName NVARCHAR(100),
+	Address NVARCHAR(100),
+	TEL INT
+)
+GO
+SELECT * FROM Company
+GO
+CREATE TABLE PRODUCTS(
+	PROID INT PRIMARY KEY IDENTITY,
+	ProName NVARCHAR(100),
+	MoTa NVARCHAR(100),
+	UNIT NVARCHAR(20),
+	Gia MONEY,
+	Qty INT
+)
+GO
+SELECT * FROM PRODUCTS
+GO
+CREATE TABLE CATEGORYS(
+	PROID INT ,
+	ComID INT,
+	Loai NVARCHAR(50)
+)
+GO
+SELECT * FROM CATEGORYS
+GO
+
+--3. Viết các câu lệnh để thêm dữ liệu vào các bảng
+INSERT INTO Company VALUES ('ASUS','USA',983232),
+							('SONY','JAPAN',854759),
+							('SAMSUNG','KOREA',221452),
+							('NOKIA','FINLAND',988587)
+GO
+INSERT INTO PRODUCTS VALUES (N'Máy Tính T450',N'Máy nhập cũ',N'Chiếc',1000,10),
+							(N'Điện Thoại Nokia5670',N'Điện thoại đang hot',N'Chiếc',200,200),
+							(N'Máy In Samsung 450',N'Máy in đang loại bình',N'Chiếc',100,10)
+GO
+INSERT INTO CATEGORYS VALUES (1,1,N'COMPUTER'),
+							(2,4,N'Điện Thoại'),
+							(3,3,N'MÁY IN')
+GO
+--4. Viết các câu lênh truy vấn để
+--a) Hiển thị tất cả các hãng sản xuất.
+SELECT ComName FROM Company
+GO
+--b) Hiển thị tất cả các sản phẩm.
+SELECT ProName FROM PRODUCTS
+GO
+--5. Viết các câu lệnh truy vấn để 
+--a) Liệt kê danh sách hãng theo thứ thự ngược với alphabet của tên.
+SELECT ComName FROM Company
+ORDER BY ComName DESC
+GO
+--b) Liệt kê danh sách sản phẩm của cửa hàng theo thứ thự giá  giảm dần.
+SELECT ProName FROM PRODUCTS
+ORDER BY PROID DESC
+GO
+--c) Hiển thị thông tin của hãng Asus.
+SELECT * FROM Company
+WHERE ComName = 'ASUS'
+GO
+--d) Liệt kê danh sách sản phẩm còn ít hơn 11 chiếc trong kho
+SELECT ProName FROM PRODUCTS
+WHERE Qty<11
+GO
+--e) Liệt kê danh sách sản phẩm của hãng Asus
+
+--6. Viết các câu lệnh truy vấn để lấy
+--a) Số hãng sản phẩm mà cửa hàng có.
+SELECT COUNT(ComName) FROM Company
+GO
+--b) Số mặt hàng mà cửa hàng bán.
+SELECT COUNT(ProName) FROM PRODUCTS
+GO
+--c) Tổng  số loại sản phẩm của mỗi hãng có trong cửa hàng.
+
+--d) Tổng số đầu sản phẩm của toàn cửa hàng
+SELECT COUNT(ProName) FROM PRODUCTS
+GO
+--7. Thay đổi những thay đổi sau trên cơ sở dữ liệu
+--a) Viết câu lệnh để  thay đổi  trường giá tiền của từng mặt hàng là dương(>0).
+ALTER TABLE PRODUCTS ADD CONSTRAINT Gia Check(Gia>0) 
+GO
+--b) Viết câu lệnh để  thay đổi  số điện thoại phải bắt đầu bằng 0.
+
+--c) Viết các câu lệnh để xác định các khóa ngoại và khóa chính của các bảng.
---
+++ lab 5/SQLQuery3.sql
@@ -0,0 +1,82 @@
+﻿CREATE DATABASE QuanLyThueBao
+GO
+CREATE TABLE CUSTOMER(
+	CusID INT Primary key IDENTITY,
+	Name NVARCHAR(100),
+	IDcard INT UNIQUE,
+	Address NVARCHAR(100)
+)
+GO
+SELECT * FROM CUSTOMER
+GO
+
+CREATE TABLE TTTB(
+	SoTB INT primary key,
+	CusID INT,
+	NgayDK Date
+	CONSTRAINT li FOREIGN KEY (CusID) REFERENCES CUSTOMER(CusID)
+)
+GO
+SELECT * FROM TTTB
+GO
+
+CREATE TABLE TTSTB(
+	SoTB INT,
+	Unit NVARCHAR(50)
+	CONSTRAINT ll FOREIGN KEY (SoTB) REFERENCES TTTB(SoTB)
+)
+GO
+SELECT * FROM TTSTB
+GO
+--3. Viết các câu lệnh để thêm dữ liệu vào các bảng
+INSERT INTO CUSTOMER VALUES (N'Nguyễn Nguyệt Nga',123456789,N'Hà Nội'),
+							(N'Trần Anh Tuấn',152487569,N'Hà Nội')
+GO
+INSERT INTO TTTB VALUES (123456789,1,'12/12/02'),
+						(123568947,2,'12/12/30')
+GO
+INSERT INTO TTSTB VALUES (123456789,N'Trả trước'),
+						(123568947,N'Trả sau')
+GO
+--4. Viết các câu lênh truy vấn để
+--a) Hiển thị toàn bộ thông tin của các khách hàng của công ty.
+SELECT * FROM CUSTOMER
+GO
+--b) Hiển thị toàn bộ thông tin của các số thuê bao của công ty.
+SELECT * FROM TTTB
+GO
+--5. Viết các câu lệnh truy vấn để lấy
+--a) Hiển thị toàn bộ thông tin của thuê bao có số: 0123456789
+SELECT * FROM TTTB WHERE SoTB = 123456789
+GO
+--b) Hiển thị thông tin về khách hàng có số CMTND: 123456789
+SELECT * FROM CUSTOMER WHERE IDcard = 123456789
+GO
+--c) Hiển thị các số thuê bao của khách hàng có số CMTND:123456789
+
+--d) Liệt kê các thuê bao đăng ký vào ngày 12/12/09
+SELECT SoTB FROM TTTB WHERE NgayDK = '12/12/09'
+GO
+--e) Liệt kê các thuê bao có địa chỉ tại Hà Nội
+
+--6. Viết các câu lệnh truy vấn để lấy
+--a) Tổng số khách hàng của công ty.
+SELECT COUNT(Name) FROM CUSTOMER
+GO
+--b) Tổng số thuê bao của công ty.
+SELECT COUNT(SoTB) FROM TTSTB
+GO
+--c) Tổng số thuê bào đăng ký ngày 12/12/09.
+SELECT COUNT(NgayDK) FROM TTTB WHERE NgayDK = '12/12/09'
+GO
+--d) Hiển thị toàn bộ thông tin về khách hàng và thuê bao của tất cả các  số thuê bao.
+
+--7. Thay đổi những thay đổi sau trên cơ sở dữ liệu
+--a) Viết câu lệnh để  thay đổi trường ngày đăng ký là not null.
+ALTER TABLE TTTB ALTER COLUMN NgayDK DATE NOT NULL;
+--b) Viết câu lệnh để  thay đổi trường ngày đăng ký là trước hoặc bằng ngày hiện tại.
+ALTER TABLE TTTB ADD CONSTRAINT NgayDK Check(NgayDK<getdate() AND NgayDK=GETDATE()) 
+GO
+--c) Viết câu lệnh để  thay đổi số điện thoại phải bắt đầu 09
+
+--d) Viết câu lệnh để thêm trường số điểm thưởng cho mỗi số thuê bao.
